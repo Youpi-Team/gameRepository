eecee42f4ce93e1900fa838f3280644bd2cb42ff
Darranar <lonatation06@orange.fr>
2016-07-15 13:09:38 +0200

    Main Menu

---
+++ YoupiGameProjectOfficial/Assets/MainMenuScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var LevelName : String = "Nom du level suivant";
+
+function Play () {
+	Application.LoadLevel(LevelName);
+
+}
+
+function Quit () {
+	Application.Quit();
+
+}
+
+
+
+
+
+
